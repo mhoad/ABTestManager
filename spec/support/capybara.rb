4348e1f680b8ac1e0152d194a9aab3af92865f24
Mark Hoad <markdhoad@gmail.com>
2020-07-12 18:08:55 +0100

    Create sign up spec

---
+++ spec/support/capybara.rb
@@ -0,0 +1,13 @@
+require 'capybara/rails'
+require 'capybara/rspec'
+Capybara.server = :puma, { Silent: true } 
+
+RSpec.configure do |config| 
+  config.before(:each, type: :system) do
+    driven_by :rack_test 
+  end
+
+  config.before(:each, type: :system, js: true) do 
+    driven_by :selenium_chrome_headless
+  end 
+end
---
+++ spec/system/create_account_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Create an account", type: :system do
+
+  before do
+    visit "/"
+    click_link "Sign up"
+    expect(current_path).to eq (new_user_registration_path)
+  end
+
+  scenario "with valid details" do
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Smith"
+    fill_in "Job title", with: "Owner"
+    fill_in "Organization", with: "HyperGlobalMegaNet"
+    fill_in "Email", with: "tester@example.tld"
+    fill_in "Password", with: "test-password"
+    fill_in "Password confirmation", with: "test-password"
+    click_button "Sign up"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+  end
+
+end
