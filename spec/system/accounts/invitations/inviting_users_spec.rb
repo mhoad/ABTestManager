ac1845a721a18750fc61c36ca1b341fe62ccf0e4
Mark Hoad <markdhoad@gmail.com>
2020-07-20 15:04:41 +0100

    Write initial invitation test

---
+++ spec/system/accounts/invitations/inviting_users_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Inviting users", type: :system do
+  let(:account) { FactoryBot.create(:account) }
+  let(:admin_user) { FactoryBot.create(:user) }
+  let(:regular_user) { FactoryBot.create(:user) }
+
+  before do
+    AddUserToAccount.call(user: admin_user, account: account, role: :admin)
+    AddUserToAccount.call(user: regular_user, account: account, role: :regular)
+  end
+
+  context "as an account admin user" do
+    before do
+      sign_in admin_user
+      visit account_prefix(account_team_path)
+    end
+  
+    scenario "creates an invitation successfully" do
+      click_link "Add User"
+      fill_in "Email", with: "test@example.com"
+      click_button "Invite User"
+  
+      expect(page).to have_content "test@example.com has been invited."
+      expect(current_path).to eq account_prefix(account_team_path)
+    end
+  end
+
+  private
+
+  def account_prefix(path)
+    "/#{AccountSlug::encode(account.slug)}#{path}"
+  end
+end
