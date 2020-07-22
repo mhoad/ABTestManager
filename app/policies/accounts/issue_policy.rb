c054998d6aa00cfba7b802071790d683f8d9f651
Mark Hoad <markdhoad@gmail.com>
2020-07-22 10:35:44 +0100

    Move common policy checks for accounts into base class to ensure that we only need to use account scoped policy classes for case by case overrides

--- app/policies/accounts/issue_policy.rb
+++ app/policies/accounts/issue_policy.rb
@@ -1,15 +1,5 @@
 module Accounts
   class IssuePolicy < BasePolicy
-    def show?
-      user_is_account_member?
-    end
-
-    def edit?
-      user_is_account_admin? || user_owns_record?
-    end
-  
-    def update?
-      user_is_account_admin? || user_owns_record?
-    end
+    
   end
 end
